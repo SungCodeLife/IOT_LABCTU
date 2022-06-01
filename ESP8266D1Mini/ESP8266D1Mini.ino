59fbc3987eeb86854878409f87a124c70c0b2573
SungCoders <levansung28@gmail.com>
2022-06-01 21:47:58 +0700

    update code esp8266D1: LED status, notification 1s

--- ESP8266D1Mini/ESP8266D1Mini.ino
+++ ESP8266D1Mini/ESP8266D1Mini.ino
@@ -1,33 +1,24 @@
-/*
- * pubsubclient version 2.8.0 by Nic O'leary 
- * when can not connect to server led on board will turn on
-*/
+#include "user_cfg.h"
 
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-/*buffer data pubish*/
-#define Buffer 240
+// variable declare
+uint32 seconds = 0;
 
-#define endln   Serial.println();
-#define show    Serial.print
-#define showln  Serial.println
-
-#define ssid_WiFi  "CTU_IOTLAB"
-#define pass_WiFi  "02710271"
-#define mqtt_server  "192.168.137.1"
-
-WiFiClient espClient;
-PubSubClient client(espClient);
+// prototype function
+void MQTT_Reconnect();
+void WIFI_SETUP();
+void SerialRead();
+void DataPublish(char topic[], String DataUser);
+inline void DebugLog_Show(String content);
 
 void setup()
 {
-  pinMode(BUILTIN_LED, OUTPUT);
-  digitalWrite(BUILTIN_LED, 0);
-  Serial.begin(115200);
-  WIFI_SETUP();
-  /* setup Server */
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+    pinMode(BUILTIN_LED, OUTPUT);
+    LED(OFF);
+    Serial.begin(BAUDRATE2);
+    WIFI_SETUP();
+    /* setup Server */
+    client.setServer(mqtt_server, 1883);
+    client.setCallback(callback);
 }
 
 void loop()
@@ -36,95 +27,95 @@ void loop()
       MQTT_Reconnect();
     client.loop();
     SerialRead();
-}
-
-void SerialRead(){
-  String DataBuffer = "";
-  while(Serial.available())
-  {
-    DataBuffer = Serial.readString();
-    if(DataBuffer != "")
+    if(millis() - seconds >= 1000)
     {
-        DataPublish("MQTT_DEBUG", DataBuffer);
-        delay(50);
-        DataPublish("MQTT_calib", DataBuffer);
+      DataPublish("MQTT_esp8266_status", String(seconds/1000));
+      seconds = millis();
     }
-  }
 }
 
-/* -----------------------Program Function-----------------------*/
-
-void WIFI_SETUP()
+void SerialRead()
 {
-  digitalWrite(BUILTIN_LED, 0);
-  delay(10);
-//  show("Connecting to " + String(ssid_WiFi));
-//  endln
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid_WiFi, pass_WiFi);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-//    show(".");
-  }
-
-//  showln("\nWiFi Connected \nIP address: ");
-//  showln(WiFi.localIP());
-}
-
-void DataPublish(char topic[], String DataUser)
-{
-  uint8_t value = 0;
-  char msg[Buffer];
-  char payload[DataUser.length()+1];
-  for(int i=0; i<= DataUser.length(); ++i)
-    payload[i] = DataUser[i];
-  snprintf(msg, Buffer, payload);
-  boolean boolPub = client.publish(topic, msg);
-//  if(!boolPub)
-    //showln("mistake to send data to server.");
+    String DataBuffer = "";
+    while(Serial.available())
+    {
+        DataBuffer = Serial.readString();
+        if(DataBuffer != "")
+        {
+            DataPublish("MQTT_DEBUG", DataBuffer);
+            delay(100);
+            DataPublish("MQTT_Data_Pub", DataBuffer);
+        }
+    }
 }
 
 void callback(char* topic, byte* payload, unsigned int length)
 {
-  String mess_payload = "";
-  String topic_convert = String(topic);
-  char msg_process[length+1];
-  /* message for MQTT*/
-  //show("Message [" + topic_convert + "]");
-  for (int i = 0; i < length; i++)
-    mess_payload += (char)payload[i];
-  //endln
-  //showln(mess_payload);
+    String mess_payload = "";
+    char msg_process[length+1];
 
-  if(topic_convert == "MQTT_read_data_sub")
-    client.publish("mqtt_status", "request read");
-  if(topic_convert == "MQTT_write_data_sub")
-    client.publish("mqtt_status", "request write");
-  if(topic_convert == "MQTT_click_config_sub")
-    client.publish("mqtt_status", "request show configuration");
-  Serial.print(mess_payload);
+    DebugLog_Show("Message [" + (String)topic + "]");
+    for (int i = 0; i < length; i++)
+        mess_payload += (char)payload[i];
+    DebugLog_Show(mess_payload);
+    UARTSEND(mess_payload);
+
 } /* end callback */
+
+/* -----------------------Program Function-----------------------*/
+void WIFI_SETUP()
+{
+    LED(OFF);
+    delay(10);
+    DebugLog_Show("\nConnecting to WiFi: " + String(ssid_WiFi) + "...");
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid_WiFi, pass_WiFi);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        LED(!READLED);
+        delay(300);
+    }
+    DebugLog_Show("\nWIFI CONNECTED");
+}
+
+void DataPublish(char topic[], String DataUser)
+{
+    uint8 value = 0;
+    char msg[Buffer];
+    char payload[DataUser.length()+1];
+    for(int i=0; i<= DataUser.length(); ++i)
+        payload[i] = DataUser[i];
+    snprintf(msg, Buffer, payload);
+    boolean boolPub = client.publish(topic, msg);
+    if(!boolPub)
+        DebugLog_Show("mistake to send data to server.");
+}
 
 void MQTT_Reconnect()
 {
   while (!client.connected())
   {
-//    show("Connecting to MQTT Server...");
+    DebugLog_Show("Connecting to MQTT Server...");
     if(WiFi.status() != WL_CONNECTED)
-      WIFI_SETUP();
+        WIFI_SETUP();
     if(client.connect("ESP"))
     {
-      digitalWrite(BUILTIN_LED, 1);
-//      showln("connected MQTT");
-      client.publish("mqtt_status", "ESP connected");
-      client.subscribe("MQTT_click_config_sub");
-      client.subscribe("MQTT_write_data_sub");
-      client.subscribe("MQTT_read_data_sub");
+        LED(OFF);
+        DebugLog_Show("CONNECTED MQTT");
+        client.subscribe("MQTT_Data_Sub");
     }
     else{
-//      showln("failed, rc = " + String(client.state()) + " try again in 5 seconds");
-      delay(5000);
+        DebugLog_Show("failed, rc = " + String(client.state()) + " try again in 5 seconds");
+        LED(ON);
+        delay(5000);
     }
   } /* end while */
 } /* end reconnect MQTT*/
+
+inline void DebugLog_Show(String content)
+{
+    #if DEBUG_LOG
+        showln(content);
+    #endif
+}
